7ca18a337d7d8423231872413ffc5040652ec737
itbeauregard <i.t.beauregard@gmail.com>
2017-08-28 23:49:15 -0400

    Create Home and Log-in pages
    Getting the hang of making jQuery blocks for each user click. Ran into
    trouble with the Bulma format for the login form--it couldn't capture
    the data from the form fields somehow. Works now.

---
+++ assets/scripts/ux.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const homePage = function () {
+  $('#warning-messages').hide()
+  $('#account-login').hide()
+  $('#create-account').hide()
+  $('#change-password').hide()
+  $('#account-signout').hide()
+  $('.floral-design-page').hide()
+  $('.csa-share-page').hide()
+  $('#get-all-requests').hide()
+  $('#reveal-buttons').hide()
+}
+
+module.exports = {
+  homePage
+}
